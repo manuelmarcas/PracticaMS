3c4558a3463992d9f2cdc31cc659a05907cceb3d
manuelmarcas <manumarcas9711@gmail.com>
2020-11-29 18:34:59 +0100

    Creados servicios admin y gateway

---
+++ gateway/src/test/java/com/practica/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
