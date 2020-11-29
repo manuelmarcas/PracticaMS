77b27547ebc4f531e07c3ced9a0d5083ac56f2e0
manuelmarcas <manumarcas9711@gmail.com>
2020-11-29 19:23:57 +0100

    Creado microservicio de factura

---
+++ factura/src/test/java/com/practica/factura/FacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.factura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
