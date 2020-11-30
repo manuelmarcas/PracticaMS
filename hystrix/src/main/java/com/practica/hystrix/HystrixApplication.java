a1e1ed85d4e8e32bda4948665361ebaae0769f05
manuelmarcas <manumarcas9711@gmail.com>
2020-11-30 01:27:36 +0100

    Añadida configuración turbine al servicio de hystrix.

--- hystrix/src/main/java/com/practica/hystrix/HystrixApplication.java
+++ hystrix/src/main/java/com/practica/hystrix/HystrixApplication.java
@@ -3,9 +3,11 @@ package com.practica.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
 public class HystrixApplication {
 
 	public static void main(String[] args) {
