5fef2b2bd17a33367fddad9f96ad8983e4afd1eb
manuelmarcas <manumarcas9711@gmail.com>
2020-11-30 18:50:37 +0100

    Configurado hystrix.

--- hystrix/src/main/java/com/practica/hystrix/HystrixApplication.java
+++ hystrix/src/main/java/com/practica/hystrix/HystrixApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
-@EnableTurbine
 public class HystrixApplication {
 
 	public static void main(String[] args) {
