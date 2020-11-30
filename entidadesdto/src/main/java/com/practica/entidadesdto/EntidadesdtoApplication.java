bf730f2cf71d2328efc68f050bdfc38034037a4c
manuelmarcas <manumarcas9711@gmail.com>
2020-11-30 19:18:11 +0100

    Creado proyecto para las entidades dto.

---
+++ entidadesdto/src/main/java/com/practica/entidadesdto/EntidadesdtoApplication.java
@@ -0,0 +1,13 @@
+package com.practica.entidadesdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadesdtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadesdtoApplication.class, args);
+	}
+
+}
