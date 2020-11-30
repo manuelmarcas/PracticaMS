c8bb9b068cad21efe015b791209459ca5c9f4b54
manuelmarcas <manumarcas9711@gmail.com>
2020-11-30 01:23:32 +0100

    Creado servicio hystrix.

---
+++ hystrix/src/test/java/com/practica/hystrix/HystrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
