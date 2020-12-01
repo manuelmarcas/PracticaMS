b7c032e8a122b16cf991916cdd2845f48f99622a
manuelmarcas <manumarcas9711@gmail.com>
2020-12-01 22:14:15 +0100

    Añadidos ficheros de propiedades en el repositorio.

--- hystrix/src/main/java/com/practica/hystrix/HystrixApplication.java
+++ hystrix/src/main/java/com/practica/hystrix/HystrixApplication.java
@@ -3,7 +3,6 @@ package com.practica.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
